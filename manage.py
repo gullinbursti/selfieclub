e73829abf7a3d48eb15ff2d4f4e5df73f55c891a
Pedro <digitalrounin@gmail.com>
2014-07-03 15:16:26 -0700

    Added an external config directory

--- manage.py
+++ manage.py
@@ -2,6 +2,10 @@
 import os
 import sys
 
+base_dir = os.path.abspath(os.path.dirname(__file__))
+config_dir = os.path.join(os.path.dirname(base_dir), 'selfieclub-config')
+sys.path.append(config_dir)
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "selfieclub.settings")
 
