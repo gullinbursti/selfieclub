80074c1a6a86b4894b50e800c4217bd7aad6d5b2
Pedro <digitalrounin@gmail.com>
2014-07-10 18:33:06 -0700

    Using ModelViewSet to make things easier

--- selfieclub/urls.py
+++ selfieclub/urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from newsfeed_user import views
+
 
 admin.autodiscover()
 
+router = DefaultRouter()
+router.register(r'^newsfeed/user/type', views.NewsfeedType)
+
 urlpatterns = patterns(
     '',
+    url(r'^', include(router.urls)),
     url(r'^media/', include('media.urls')),
     url(r'^activity/', include('activity.urls')),
-    url(r'^newsfeed/user/', include('newsfeed_user.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     )
