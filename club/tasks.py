cec622ae166c1d2383245d2569859dafdc49e8b1
Pedro <digitalrounin@gmail.com>
2014-07-28 17:04:41 -0700

    Minor changes to test with

--- club/tasks.py
+++ club/tasks.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from celery import shared_task
+import member
 import logging
 
 logger = logging.getLogger(__name__)
@@ -10,6 +11,14 @@ logger = logging.getLogger(__name__)
 def invitation_sent(club_id, actor_member_id, invitee_member_id, when):
     logger.info("Event received: invitation_sent({}, {}, {}, {})"
                 .format(club_id, actor_member_id, invitee_member_id, when))
+
+    if not user_exists(actor_member_id):
+        logger.debug("Actor '{}' does not exist".format(actor_member_id))
+        return
+
+    if not user_exists(invitee_member_id):
+        logger.debug("Invitee '{}' does not exist".format(invitee_member_id))
+        return
 
 
 @shared_task
@@ -28,3 +37,7 @@ def quit(club_id, actor_member_id, when):
 def post_status_update(club_id, actor_member_id, selfie_id, when):
     logger.info("Event received: post_status_update")
     pass
+
+
+def user_exists(pk):
+    member.models.Member.objects.filter(pk=pk).exists()
