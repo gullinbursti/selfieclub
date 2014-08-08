1b1e5564a732609b3d39cccd942d0689f23eb66e
Pedro <digitalrounin@gmail.com>
2014-08-07 18:02:19 -0700

    Adding clubnames

--- club/serializers.py
+++ club/serializers.py
@@ -15,14 +15,26 @@ class Club(serializers.ModelSerializer):
         fields = ('id', 'name', 'club_type', 'owner', 'description', 'img', 'added')
 
 
+class ClubSummary(serializers.ModelSerializer):
+    class Meta:
+        model = models.Club
+        fields = ('id', 'name')
+
+
 class ClubLabel(serializers.ModelSerializer):
+    clubs = ClubSummary(
+        many=True,
+        source='club',
+        read_only=True
+    )
     club_ids = serializers.SlugRelatedField(
         source='club',
         slug_field='id',
         many=True,
+        write_only=True,
         widget=widgets.TextInput, help_text='Comma delimited array of club_ids.'
     )
 
     class Meta:
         model = models.ClubLabel
-        fields = ('id', 'name', 'description', 'club_ids', 'updated', 'created')
+        fields = ('id', 'name', 'description', 'clubs', 'club_ids', 'updated', 'created')
