c10179e7b258d37421867553cd8505bf39eeabd0
Phillip Winn <phillip@builtinmenlo.com>
2014-09-30 04:35:37 +0000

    Adding messaging to tox checks and cleaning up to pass all but one (broken) check.

--- messaging/amazon.py
+++ messaging/amazon.py
@@ -1,3 +1,5 @@
+"""Send messages via the Amazon SNS service."""
+
 import boto
 from boto import sns
 from django.conf import settings
@@ -5,47 +7,41 @@ import json
 import re
 
 # APNS for production. APNS_SANDBOX for dev
-environment = settings.AMAZON_SNS_ENV
+ENVIRONMENT = settings.AMAZON_SNS_ENV
 # One for dev, another for production
-platform_arn = settings.AMAZON_SNS_ARN
-amazonsns = sns.SNSConnection(settings.AWS_CREDENTIALS['key'],
+PLATFORM_ARN = settings.AMAZON_SNS_ARN
+AMAZONSNS = sns.SNSConnection(settings.AWS_CREDENTIALS['key'],
                               settings.AWS_CREDENTIALS['secret'])
 
 
 def send_push_message(device_token, message_text, payload=None):
-    """Shortcut to send a push message using Amazon's SNS API
-
-    Usage:
-
-    >>> send_push_message(device_token, 'My push message text')
-    """
+    """Shortcut to send a push message using Amazon's SNS API."""
     response = None
-    # idempotent call to create/retrieve deviceArn for this token
+    # idempotent call to create/retrieve device_arn for this token
     try:
-        dResponse = amazonsns.create_platform_endpoint(
-            platform_arn, device_token, None,
-            {u'Token': device_token, u'Enabled': True})
-    except boto.exception.BotoServerError, e:
+        d_rspnse = AMAZONSNS.create_platform_endpoint(PLATFORM_ARN,
+                                                      device_token, None,
+                                                      {u'Token': device_token,
+                                                       u'Enabled': True})
+    except boto.exception.BotoServerError as exception:
         match = re.match(".*Endpoint (?P<arn>.*?) already.*",
-                         e.message)
-        amazonsns.delete_endpoint(match.group('arn'))
-    dResponse = amazonsns.create_platform_endpoint(
-        platform_arn, device_token, None,
+                         exception.message)
+        AMAZONSNS.delete_endpoint(match.group('arn'))
+    d_rspnse = AMAZONSNS.create_platform_endpoint(
+        PLATFORM_ARN, device_token, None,
         {u'Token': device_token, u'Enabled': True})
-    if 'CreatePlatformEndpointResponse' in dResponse:
-        deviceArn = dResponse[
-            'CreatePlatformEndpointResponse'][
-            'CreatePlatformEndpointResult'][
-            'EndpointArn']
+    if 'CreatePlatformEndpointResponse' in d_rspnse:
+        device_arn = d_rspnse['CreatePlatformEndpointResponse'][
+            'CreatePlatformEndpointResult']['EndpointArn']
         apns_dict = {'aps': {'alert': message_text, 'sound': 'default'}}
         for key, value in payload.items():
             apns_dict['aps'][key] = value
         apns_string = json.dumps(apns_dict, ensure_ascii=False)
-        message = {'default': message_text, environment: apns_string}
-        messageJSON = json.dumps(message, ensure_ascii=False)
+        message = {'default': message_text, ENVIRONMENT: apns_string}
+        message_json = json.dumps(message, ensure_ascii=False)
 
-        response = amazonsns.publish(
-            message=messageJSON,
-            target_arn=deviceArn,
+        response = AMAZONSNS.publish(
+            message=message_json,
+            target_arn=device_arn,
             message_structure='json')
     return response
