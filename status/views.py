199833757fbfb97a65a904e01dc082a4d3533d95
Phillip Winn <phillip@builtinmenlo.com>
2014-10-14 15:07:23 +0000

    Better RESTful endpoints

--- status/views.py
+++ status/views.py
@@ -3,23 +3,27 @@ from status import models
 from rest_framework import viewsets
 
 
-class StatusUpdate(viewsets.ReadOnlyModelViewSet):
-    # pylint exception - inherited from Django parent
-    # pylint: disable=too-many-public-methods, too-many-ancestors
-    queryset = models.StatusUpdate.objects.all()
-    serializer_class = serializers.StatusUpdate
-
-    def get_queryset(self):
-        pass
-
-
 class StatusUpdateViewSet(viewsets.ReadOnlyModelViewSet):
     # pylint exception - inherited from Django parent
     # pylint: disable=too-many-public-methods, too-many-ancestors
-    # queryset = models.StatusUpdate.objects.all()
     serializer_class = serializers.StatusUpdate
     model = models.StatusUpdate
 
     def get_queryset(self):
-        update_id = self.kwargs['pk']
+        if 'pk' in self.kwargs:
+            update_id = self.kwargs['pk']
+            queryset = models.StatusUpdate.objects.filter(id=update_id)
+        else:
+            queryset = models.StatusUpdate.objects.all()
+        return queryset
+
+
+class StatusUpdateTraffic(viewsets.ReadOnlyModelViewSet):
+    # pylint exception - inherited from Django parent
+    # pylint: disable=too-many-public-methods, too-many-ancestors
+    serializer_class = serializers.StatusUpdate
+    model = models.StatusUpdate
+
+    def get_queryset(self):
+        update_id = self.kwargs['update_id']
         return models.StatusUpdate.objects.filter(id=update_id)
