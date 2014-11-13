e5c23ff61e883b3a7949d729e8994723813f758a
Phillip Winn <phillip@builtinmenlo.com>
2014-11-13 01:01:00 +0000

    First pass at voting

--- status/serializers.py
+++ status/serializers.py
@@ -24,3 +24,23 @@ class StatusUpdateViewerSerializer(serializers.ModelSerializer):
         # pylint: disable=too-few-public-methods
         model = models.StatusUpdateViewer
         fields = ('member_id', 'status_update_id')
+
+
+class StatusUpdateVoterSerializer(serializers.ModelSerializer):
+    member_id = serializers.IntegerField(
+        widget=widgets.TextInput,
+        source='member_id',
+        help_text='Enter member_id.')
+    vote = serializers.IntegerField(
+        widget=widgets.TextInput,
+        source='vote',
+        help_text='Enter vote.')
+    status_update_id = serializers.IntegerField(
+        widget=widgets.TextInput,
+        source='status_update_id',
+        help_text='This value is ignored in POSTs, pulled from URL.')
+
+    class Meta(object):
+        # pylint: disable=too-few-public-methods
+        model = models.StatusUpdateVoter
+        fields = ('member_id', 'vote', 'status_update_id')
