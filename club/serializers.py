50f3444b83696c852f92e8a4c91969ac726ede71
Pedro H <pedro@builtinmenlo.com>
2014-12-09 15:28:54 -0800

    Added total_status_updates and total_members

--- club/serializers.py
+++ club/serializers.py
@@ -1,8 +1,9 @@
 from club import models
+from datetime import datetime
 from django.forms import widgets
-# from django.forms.models import model_to_dict
+from member import models as member_models
+from status import models as status_models
 from rest_framework import serializers
-# import json
 
 
 class ClubType(serializers.ModelSerializer):
@@ -20,7 +21,24 @@ class GeoCoordinateSerializer(serializers.Serializer):
 
 class Club(serializers.ModelSerializer):
     # pylint: disable=too-few-public-methods
-    coords = GeoCoordinateSerializer(read_only=True)
+    coords = GeoCoordinateSerializer(read_only=True, source='*')
+    total_members = serializers.IntegerField(read_only=True, source='*')
+    total_status_updates = serializers.IntegerField(read_only=True, source='*')
+
+    def transform_total_members(self, obj, value):
+        # pylint: disable=no-self-use,unused-argument
+        # Note that club_member.joined is set to '0000-00-00 00:00:00' if the
+        # member has not actually joined the club.  '0000-00-00 00:00:00' is an
+        # invalid date and time.  Searching greater then '0001-01-01'.
+        count = member_models.ClubMember.objects.filter(club=obj.id) \
+            .filter(joined__gte=datetime(1, 1, 1)).count()
+        # +1 to include the owner
+        return count + 1
+
+    def transform_total_status_updates(self, obj, value):
+        # pylint: disable=no-self-use,unused-argument
+        count = status_models.StatusUpdate.objects.filter(club=obj.id).count()
+        return count
 
     def transform_coords(self, obj, value):
         # pylint: disable=no-self-use,unused-argument
@@ -29,7 +47,7 @@ class Club(serializers.ModelSerializer):
     class Meta(object):
         model = models.Club
         fields = ('id', 'name', 'club_type', 'owner', 'description', 'img',
-                  'coords', 'added')
+                  'total_members', 'total_status_updates', 'coords', 'added')
 
 
 class ClubSummary(serializers.ModelSerializer):
